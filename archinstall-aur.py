1046b1ca6ff33d14a9fb37b1b050208eda9ca011
Anton Hvornum <anton.feeds@gmail.com>
2021-04-29 14:23:07 +0200

    Added example on_<event> hook. In this case on_pacstrap that will be called during installation.pacstrap() calls.

--- archinstall-aur.py
+++ archinstall-aur.py
@@ -1,5 +1,14 @@
 import archinstall
 
+class PluginHandler():
+	def on_pacstrap(self, packages :list) -> list:
+		print(f"Dealing with AUR packages: {packages}")
+
+		# Returns a curated list of packages that exludes any AUR packages.
+		# This allows installation.pacstrap() to contain AUR packages,
+		# but won't handle them or try to install them since we remove those here.
+		return packages
+
 def install_aur_packages(*packages):
 	if len(packages) == 1 and type(packages[0]) == list:
 		packages = packages[0]
@@ -10,3 +19,4 @@ def install_aur_packages(*packages):
 
 # Example function injection
 archinstall.install_aur_packages = install_aur_packages
+archinstall.plugins['AUR'] = PluginHandler()
