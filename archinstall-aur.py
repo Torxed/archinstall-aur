6bae5f14dca32b357bbb772aec66d3918f5f7a01
Anton Hvornum <anton.feeds@gmail.com>
2021-05-24 13:29:13 +0200

    Changed plugin approach to use built-in entrypoints.

--- archinstall-aur.py
+++ archinstall-aur.py
@@ -1,22 +1,18 @@
 import archinstall
 
-class PluginHandler():
+class Plugin():
 	def on_pacstrap(self, packages :list) -> list:
-		print(f"Dealing with AUR packages: {packages}")
+		print(f"Identifying AUR packages in package list: {packages}")
+
+		# ... do AUR work on the packages ...
 
 		# Returns a curated list of packages that exludes any AUR packages.
 		# This allows installation.pacstrap() to contain AUR packages,
 		# but won't handle them or try to install them since we remove those here.
 		return packages
 
-def install_aur_packages(*packages):
-	if len(packages) == 1 and type(packages[0]) == list:
-		packages = packages[0]
-	elif len(packages) == 1 and type(packages[0]) == str:
-		packages = packages[0].split(' ')
-
-	print('Installing AUR packages:', packages)
+def dummy_example(*args, **kwargs):
+	pass
 
 # Example function injection
-archinstall.install_aur_packages = install_aur_packages
-archinstall.plugins['AUR'] = PluginHandler()
+archinstall.plugin_function = dummy_example()
